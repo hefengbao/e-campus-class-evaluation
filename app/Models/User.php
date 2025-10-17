5f52303bac22a55ff11987388d06f153fa004b2d
hefengbao <754582231@qq.com>
2025-10-17 15:49:02 +0800

    add dept() relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -22,6 +23,18 @@ class User extends Authenticatable implements FilamentUser
         'password',
     ];
 
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    public function dept(): BelongsTo
+    {
+        return $this->belongsTo(Department::class, 'dept_id');
+    }
+
     public function canAccessPanel(Panel $panel): bool
     {
         if ($panel->getId() === 'admin') {
@@ -32,12 +45,5 @@ class User extends Authenticatable implements FilamentUser
         }
 
         return true;
-    }
-
-    protected function casts(): array
-    {
-        return [
-            'password' => 'hashed',
-        ];
     }
 }
