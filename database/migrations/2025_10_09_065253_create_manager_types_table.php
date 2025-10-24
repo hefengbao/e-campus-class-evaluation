564028bd2e507b1e9425085d5aefc982c0df9042
hefengbao <754582231@qq.com>
2025-10-24 10:05:01 +0800

    add scope column

--- database/migrations/2025_10_09_065253_create_manager_types_table.php
+++ database/migrations/2025_10_09_065253_create_manager_types_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('manager_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->tinyText('scope')->nullable()->comment('查询范围');
             $table->timestamps();
         });
     }
