ffe25ebb27e08aa0cefc7e6c6abd9c0bbff1b5c4
hefengbao <754582231@qq.com>
2025-10-30 18:09:28 +0800

    widget view

---
+++ resources/views/filament/admin/widgets/welcome-widget.blade.php
@@ -0,0 +1,6 @@
+<x-filament-widgets::widget>
+    <x-filament::section>
+        {{-- Widget content --}}
+        欢迎使用{{ config('app.name') }}。
+    </x-filament::section>
+</x-filament-widgets::widget>
