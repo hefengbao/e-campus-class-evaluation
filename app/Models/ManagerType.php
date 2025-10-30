7d55e7db7f137b0f44b15740f7f9a9751648b0bd
hefengbao <754582231@qq.com>
2025-10-30 18:10:33 +0800

    添加 scope 字段可填充

--- app/Models/ManagerType.php
+++ app/Models/ManagerType.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class ManagerType extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'scope'];
 
     public function managers(): HasMany
     {
