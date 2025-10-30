96fe1dc308abe970a8693698464b6619ac598fac
hefengbao <754582231@qq.com>
2025-10-30 18:08:01 +0800

    自定义 Dashboard

---
+++ app/Filament/App/Widgets/WelcomeWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\App\Widgets;
+
+use Filament\Widgets\Widget;
+
+class WelcomeWidget extends Widget
+{
+    protected string $view = 'filament.app.widgets.welcome-widget';
+}
